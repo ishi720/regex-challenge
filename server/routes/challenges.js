a4deb3a7dc71e2dc05f8fba8bc8f1060377e4404
ishi720 <ishizuka720@gmail.com>
2025-07-15 22:46:35 +0900

    チャレンジ単体を取得するAPI

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+// チャレンジID指定で単体取得
+router.get('/:id', async (req, res) => {
+  const challengeId = req.params.id;
+
+  try {
+    const [rows] = await db.query('SELECT * FROM challenges WHERE id = ?', [challengeId]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'チャレンジが見つかりません' });
+    }
+
+    res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'DB Error' });
+  }
+});
+
 export default router;
